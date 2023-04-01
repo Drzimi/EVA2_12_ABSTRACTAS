bd834c4d574720d56df336a7fdaddef7d87f5a80
Drzimi <124393239+Drzimi@users.noreply.github.com>
2023-03-31 21:56:42 -0600

    Add files via upload

---
+++ EVA2_12_ABSTRACTAS/src/eva2_12_abstractas/EVA2_12_ABSTRACTAS.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva2_12_abstractas;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA2_12_ABSTRACTAS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Prueba prueba = new Prueba();
+        PruebaAbstracta pruebaAbs = new PruebaAbstracta();
+    }
+    
+}
+class Prueba{
+    
+}
+abstract class PruebaAbstracta{
+    
+}
